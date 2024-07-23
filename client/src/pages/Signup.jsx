d40b65615f5dc35c2e69972da5f10ac9695d61bd
Bhavnish Patel <patelbhavnish92@gmail.com>
2024-07-23 17:48:24 +0530

    Made Designs Of  LogIn and SignUp Page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,77 @@
-
+import {Link} from "react-router-dom"
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="mt-10 sm:mt-20 min-h-screen flex items-center justify-center w-full">
+      <div className="bg-white shadow-md rounded-3xl px-5 py-6 w-full sm:w-[27vw]">
+        <h1 className="text-2xl font-bold text-center mb-4">Let's Connect!</h1>
+        <form>
+          {/* For UserName */}
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="bhavnishPatel"
+              className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
+          </div>
+          {/* For Email */}
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="your@email.com"
+              className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
+          </div>
+          {/* For Password */}
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
+          </div>
+          {/* For Account Selection */}
+          <div className="mb-4">
+            <label
+              htmlFor="accountType"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Select User Account Type
+            </label>
+            <select className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black" >
+              <option  value="buyer">Buyer</option>
+              <option value="seller">Seller</option>
+            </select>
+          </div>
+          {/* Login With Account */}
+          <div className="flex items-center justify-end mb-4">
+          <Link to="/login" className="text-xs text-black ">Log In With Account</Link>
+          </div>
+          <button className="w-full py-2 px-4 rouded-md shadow-md text-sm font-medium text-white  bg-black" type="submit">Signup</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
